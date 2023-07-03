f79be00601c5d40a9965061259ce55ac6c3b242a
Firoj Mansuri <firoj.mansuri@rws.com>
2023-07-03 15:36:46 +0530

    intial server setup

---
+++ src/config/app.ts
@@ -0,0 +1,13 @@
+
+import express, { Request, Response} from "express"
+import bodyParser from "body-parser"
+import cors  from "cors"
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(port, () =>{
+     console.log(`Example app listening on port http://localhost:${port}!`)
+    })
