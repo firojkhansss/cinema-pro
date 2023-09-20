b6f07fc677d34f731871d6e8e1c6d2c8e4e3d837
Firoj Mansuri <firoj.mansuri@rws.com>
2023-09-20 12:18:52 +0530

    here is the

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,6 +3,8 @@ import express, { Request, Response} from "express"
 import bodyParser from "body-parser"
 import cors  from "cors"
 const app = express()
+// here is the demo
+
 const port = 3000
 
 app.use(bodyParser.json())
